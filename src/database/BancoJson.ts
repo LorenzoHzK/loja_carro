24576552c5fb107f85393ea87709390a5cb186c6
LorenzoHzK <matheuslorenzosiquiera@gmail.com>
2026-06-09 20:26:35 -0300

    feat: initialize vendas.json with sample data and refactor BancoJson methods

--- src/database/BancoJson.ts
+++ src/database/BancoJson.ts
@@ -4,13 +4,10 @@ import path from "path";
 export default abstract class BancoJson<T> {
   constructor(protected readonly caminhoArquivo: string) {}
 
-  protected salvarLista(dados: T[]): void {
-    fs.mkdirSync(path.dirname(this.caminhoArquivo), { recursive: true });
-    fs.writeFileSync(
-      this.caminhoArquivo,
-      JSON.stringify(dados, null, 2),
-      "utf-8",
-    );
+  protected adicionar(item: T): void {
+    const dados = this.carregarLista();
+    dados.push(item);
+    this.salvarLista(dados);
   }
 
   protected carregarLista(): T[] {
@@ -27,9 +24,12 @@ export default abstract class BancoJson<T> {
     return JSON.parse(conteudo) as T[];
   }
 
-  protected adicionar(item: T): void {
-    const dados = this.carregarLista();
-    dados.push(item);
-    this.salvarLista(dados);
+  protected salvarLista(dados: T[]): void {
+    fs.mkdirSync(path.dirname(this.caminhoArquivo), { recursive: true });
+    fs.writeFileSync(
+      this.caminhoArquivo,
+      JSON.stringify(dados, null, 2),
+      "utf-8",
+    );
   }
 }
